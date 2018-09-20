a752aa325098b2b220bd707cf46539a674279b0c
benben <benben@benbenli>
2018-09-20 10:08:24 +0800

    ss

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ App({
       //调用登录接口
       wx.login({
         success: function () {
-          wx.getUserInfo({
+          wx.getUserInfo({   
             success: function (res) {
               that.globalData.userInfo = res.userInfo;
               typeof cb == "function" && cb(that.globalData.userInfo)
